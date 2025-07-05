c41a98cf54a4354ddb0135b9fc89abe67b4df5e6
Daniel <josedaniel.jarap@gmail.com>
2025-07-05 09:45:30 -0500

    header refactored

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,72 +5,75 @@ import panImg from "public/pan-arabe.jpeg"
 import logoImg from "public/minilogo.png.webp"
 import phoneImg from "public/phone-icon.png"
 import cartImg from "public/cart.png"
+import testImg from "public/test-bread.jpeg"
 import { quicksand } from "utils/fonts"
-
 import { useState } from 'react'
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <main className={`bg-amber-100 text-gray-800 ${quicksand.className}`}>
+    <main className={`${quicksand.className}`}>
 
-      {/* Header */}
-      <header className="bg-black flex items-center justify-between px-4 py-2">
-        <div className="flex items-center gap-1 cursor-pointer">
+      {/* Logo and bakery name */}
+      <header className="bg-black flex items-center justify-between">
+        <div className="flex items-center gap-1 cursor-pointer hover:bg-zinc-800 active:bg-zinc-800 px-4 py-2">
           <Image 
             src={logoImg}
             alt="pan icon"
             width={28}
-            className="md:w-7"
+            className="object-cover"
           />
           <h1 className="text-white text-md md:text-md font-semibold">Panadería Moisés</h1>
         </div>
 
         {/* Navbar for desktop */}
-        <nav className="hidden md:flex md:gap-5">
-          <div className="flex gap-1 cursor-pointer">
+        <nav className="hidden md:flex h-11">
+          <div className="flex gap-1 cursor-pointer hover:bg-zinc-800 px-4 py-2">
             <Image 
               src={phoneImg}
               alt="phone icon"
-              width={23}
+              width={28}
+              className="object-cover"
             />
             <h2 className="text-white text-md font-semibold">Contáctanos</h2>
           </div>
-          <div className="flex gap-1 cursor-pointer">
+          <div className="flex gap-1 cursor-pointer hover:bg-zinc-800 px-4 py-2">
             <Image
               src={cartImg}
               alt="cart icon"
-              width={23}
+              width={28}
+              className="object-cover"
             />
             <h2 className="text-white text-md font-semibold">Carrito</h2>
           </div>
         </nav>
 
-        {/* Navbar for mobile */}
+        {/* Mobile navbar icon */}
         <div
-          className="md:hidden cursor-pointer w-6 h-6 relative"
+          className="md:hidden cursor-pointer p-4"
           onClick={() => setIsOpen(!isOpen)}
         >
           {/* Línea 1 */}
-          <span
-            className={`absolute h-1 w-6 bg-zinc-200 rounded-full transition-transform duration-300 ease-in-out 
-            ${isOpen ? 'rotate-45 top-2.5' : 'top-0'}`}
-          />
+          <div
+            className={` h-1 w-6 bg-zinc-200 rounded-full transition-transform relative
+            ${isOpen ? 'rotate-45 top-2' : 'top-0'}`}>
+          </div>
           {/* Línea 2 */}
-          <span
-            className={`absolute h-1 w-6 bg-zinc-200 rounded-full transition-all duration-300 ease-in-out 
-            ${isOpen ? 'opacity-0' : 'top-2.5'}`}
-          />
+          <div
+            className={` mt-1 h-1 w-6 bg-zinc-200 rounded-full transition-all  
+            ${isOpen ? 'opacity-0' : ''}`}>
+          </div>
           {/* Línea 3 */}
-          <span
-            className={`absolute h-1 w-6 bg-zinc-200 rounded-full transition-transform duration-300 ease-in-out 
-            ${isOpen ? '-rotate-45 bottom-2.5' : 'bottom-0'}`}
-          />
+          <div
+            className={` mt-1 h-1 w-6 bg-zinc-200 rounded-full transition-transform relative
+            ${isOpen ? '-rotate-45 bottom-2' : 'bottom-0'}`}>
+          </div>
         </div>
-
+        
+        {/* mobile navbar */}
         {isOpen && (
-          <ul className="md:hidden absolute top-10 right-0 w-50 bg-zinc-900 text-white">
+          <ul className="md:hidden absolute top-13 right-0 w-50 bg-zinc-900 text-white z-10">
             <li className="relative flex items-center justify-center cursor-pointer p-4 font-semibold hover:bg-zinc-800 active:bg-zinc-800">
               <div className="flex gap-1 cursor-pointer">
                 <Image 
@@ -93,26 +96,24 @@ export default function Home() {
             </li>
           </ul>
         )}
-        
-
-
       </header>
 
 
 
 
       {/* Hero Section */}
-      <section className="text-center py-12 px-5">
+      <section className="text-center py-12 px-5 relative">
         <h1 className="text-4xl md:text-5xl font-bold mb-4">Pan Integral Artesanal</h1>
-        <p className="text-xl md:text-2xl mb-6">
-          Natural, saludable y <span className="text-yellow-700 font-semibold">sin manteca</span>.
+        <p className="text-xl md:text-2xl mb-6 font-semibold">
+          Natural, saludable y <span className="text-yellow-200 font-semibold">sin manteca</span>.
         </p>
         <Image
-          src={panImg}
+          src={testImg}
           alt="Pan integral sin manteca"
-          className="mx-auto rounded-2xl shadow-lg w-full max-w-md"
+          fill
+          className="object-cover -z-10"
         />
-        <button className="mt-6 px-6 py-3 bg-yellow-700 text-white rounded-xl hover:bg-yellow-800 active:bg-yellow-800 cursor-pointer">
+        <button className="mt-6 px-6 py-3 bg-yellow-700 text-white rounded-xl hover:bg-yellow-800 active:bg-yellow-800 cursor-pointer font-semibold">
           ¡Quiero probarlo!
         </button>
       </section>
